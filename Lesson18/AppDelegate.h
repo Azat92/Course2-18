be139ca42e4cb035ac67b6159141982f3884c1ef
Azat Almeev <azat.almeev@gmail.com>
2016-04-09 23:58:41 +0300

    Initial Commit

---
+++ Lesson18/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson18
+//
+//  Created by Azat Almeev on 09.04.16.
+//  Copyright © 2016 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
